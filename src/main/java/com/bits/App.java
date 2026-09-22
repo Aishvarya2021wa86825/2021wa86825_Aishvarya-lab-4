8d5860fb5182d7de9398ca780ac097390d34a474
Aishvarya_2021wa86825 <2021wa86825@wilp.bits-pilani.ac.in>
2026-09-22 12:45:32 +0530

    Fix Java source for Jenkins CI

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -4,10 +4,4 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from BITS Jenkins CI Project - Jenkins CI Test");
     }
-}System.out.println("Hello World - Jenkins CI Test");package com.bits;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from BITS Jenkins CI Project");
-    }
 }
